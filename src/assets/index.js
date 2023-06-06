d983bc82e47fcf066435dafe0259a126fc3dfc3b
Siddharth Shankar <sidshnkar@gmail.com>
2023-06-07 01:40:44 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -58,7 +58,7 @@ import postman_logo from "./postman_logo.png";
 import parsec_logo from "./parsec_logo.jpeg";
 import inter_iit_logo from "./inter_iit_logo.png";
 import reddit_kubernetes_logo from "./kubernetes_reddit_logo.png"; 
-import aws_logo from "./aws_logo.png";
+import aws_logo from "./aws_logo.jpg";
 
 export {
   airbnb,
